9f7178c8fa4687bd13a3c991ac9a92e295c3d47b
Ives Chiba <ivesjundi@gmail.com>
2024-03-07 17:11:31 -0300

    Criação dos componentes Button, Formulario e Lista

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// class Botao extends React.Component {
+//     render(): React.ReactNode {
+//         return (
+//             <button>Botão</button>
+//         )
+//     }
+// }
+
+
+// export default Botao;
+
+export const Botao = () => {
+  return (
+    <button>Botão</button>
+  )
+}
